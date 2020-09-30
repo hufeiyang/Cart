0d8e05a31017f71fb21d7ecedbc52316bfe270fb
hufy@akulaku.com <hufy@akulaku.com>
2020-09-30 11:50:28 +0800

    添加组件：export_cart

---
+++ export_cart/src/main/java/com/hfy/export_cart/router/CartRouterTable.java
@@ -0,0 +1,20 @@
+package com.hfy.export_cart.router;
+
+/**
+ * 购物车组件路由表
+ * 即 购物车组件中 所有可以从外部跳转的页面 的路由信息
+ * @author hufeiyang
+ */
+public interface CartRouterTable {
+
+    /**
+     * 购物车页面
+     */
+    String PATH_PAGE_CART = "/cart/cartActivity";
+
+    /**
+     * 购物车服务
+     */
+    String PATH_SERVICE_CART = "/cart/service";
+
+}
