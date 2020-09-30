cfb80bfcaef768f01fd4fa27cb467a532f79a533
hufy@akulaku.com <hufy@akulaku.com>
2020-09-30 15:55:51 +0800

    组件cart 提供服务

--- module_cart/src/main/java/com/hfy/module_cart/CartActivity.java
+++ module_cart/src/main/java/com/hfy/module_cart/CartActivity.java
@@ -1,12 +1,12 @@
 package com.hfy.module_cart;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.hfy.export_cart.router.CartRouterTable;
 
-@Route(path = "/cart/cartActivity")
+@Route(path = CartRouterTable.PATH_PAGE_CART)
 public class CartActivity extends AppCompatActivity {
 
     @Override
---
+++ module_cart/src/main/java/com/hfy/module_cart/CartServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hfy.module_cart;
+
+import android.content.Context;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.hfy.export_cart.router.CartRouterTable;
+import com.hfy.export_cart.service.ICartService;
+
+/**
+ * 购物车组件服务的实现
+ * @author hufeiyang
+ */
+@Route(path = CartRouterTable.PATH_SERVICE_CART)
+public class CartServiceImpl implements ICartService {
+
+    @Override
+    public int getProductCountInCart() {
+        return 666;
+    }
+
+    @Override
+    public void init(Context context) {
+        //初始化工作，服务注入时会调用
+    }
+}
