165e2d8df7404f54c27595584f4c299eb9b7427d
hufy@akulaku.com <hufy@akulaku.com>
2020-10-02 19:21:59 +0800

    module_cart 新增CartFragment

--- module_cart/src/main/java/com/hfy/module_cart/CartActivity.java
+++ module_cart/src/main/java/com/hfy/module_cart/CartActivity.java
@@ -1,6 +1,10 @@
 package com.hfy.module_cart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -13,5 +17,12 @@ public class CartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction  transaction= manager.beginTransaction();
+//        Fragment userFragment = CartFragment.newInstance("param1","param2");
+//        transaction.add(R.id.fl_test_fragment, userFragment, "tag");
+//        transaction.commit();
     }
 }
---
+++ module_cart/src/main/java/com/hfy/module_cart/CartFragment.java
@@ -0,0 +1,52 @@
+package com.hfy.module_cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.hfy.export_cart.CartServiceUtil;
+import com.hfy.export_cart.router.CartRouterTable;
+
+
+//@Route(path = CartRouterTable.PATH_FRAGMENT_CART)
+public class CartFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+    public static CartFragment newInstance(String param1, String param2) {
+        CartFragment fragment = new CartFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
